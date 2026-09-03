b1b0e539762ae295749e19cd3d9ea80108f93f3f
Hiram GG <reaxorgg@gmail.com>
2026-09-03 15:18:18 -0500

    Resultado por ciudad

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -11,5 +11,14 @@ const clientesSimples = clientes.map(cliente => ({
   console.log('==Directorio de clientes==');
   console.log(clientesSimples);
  console.log('==Clientes con email .biz==');
-const activeClients = clientes.filter(client => client.email.endsWith(".biz"));
+const activeClients = clientes.filter(cliente => cliente.email.endsWith(".biz"));
 activeClients.forEach(cliente => {console.log(`Cliente: ${cliente.name}| Ciudad: ${cliente.address.city}|Empresa: ${cliente.company.name}`)})
+
+const clientesXciudad = clientes.reduce((acc, cliente) => {
+    const city = cliente.address.city;
+    acc[city] = (acc[city] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  console.log('==Clientes por ciudad==');
+  console.log(clientesXciudad);
