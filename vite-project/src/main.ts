8470f2cfa5d7ef886b6121f613368ca1ac58aae4
Hiram GG <reaxorgg@gmail.com>
2026-09-03 15:13:16 -0500

    formato dos primeros

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -2,13 +2,14 @@ import { getUsers } from './services/userService'
 
 const clientes = await getUsers();
 
+const clientesSimples = clientes.map(cliente => ({
+    id: cliente.id,
+    nombre: cliente.name,
+    correo: cliente.email,
+    empresa: cliente.company.name
+  }));
+  console.log('==Directorio de clientes==');
+  console.log(clientesSimples);
+ console.log('==Clientes con email .biz==');
 const activeClients = clientes.filter(client => client.email.endsWith(".biz"));
-
-console.log('Clientes:', activeClients);
-
-console.log(clientes);
-
-
-clientes.forEach(cliente => {
-    console.log(`Cliente: ${cliente.id} - Categoría: ${cliente.name} - Correo electronico: ${cliente.email} - Ciudad ${cliente.address.city} - Empresa ${cliente.company.name}`);
-  });
+activeClients.forEach(cliente => {console.log(`Cliente: ${cliente.name}| Ciudad: ${cliente.address.city}|Empresa: ${cliente.company.name}`)})
