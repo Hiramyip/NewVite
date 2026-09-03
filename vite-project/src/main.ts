47d94986e5a90b7b8dda6ca0922e6fd30ec92b62
Hiram GG <reaxorgg@gmail.com>
2026-09-03 15:00:59 -0500

    Priimer requerimiento

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,5 +1,14 @@
 import { getUsers } from './services/userService'
 
 const clientes = await getUsers();
+
+const activeClients = clientes.filter(client => client.email.endsWith(".biz"));
+
+console.log('Clientes:', activeClients);
+
 console.log(clientes);
 
+
+clientes.forEach(cliente => {
+    console.log(`Cliente: ${cliente.id} - Categoría: ${cliente.name} - Correo electronico: ${cliente.email} - Ciudad ${cliente.address.city} - Empresa ${cliente.company.name}`);
+  });
